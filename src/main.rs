491aec15dc807f459995cab40e9c6f41b1821345
Ananth Raghav <raghavananth02@gmail.com>
2023-06-03 19:41:39 +0600

    Made configuration changes 2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use password::generate_password;
 use copypasta::{ClipboardContext, ClipboardProvider};
 
 #[derive(Parser, Debug)]
-#[command(author=env!("CARGO_PKG_AUTHORS"), version=env!("CARGO_PKG_VERSION"), about="A simple command line password generator", long_about = None)]
+#[command(author=env!("CARGO_PKG_AUTHORS"), version=env!("CARGO_PKG_VERSION"), about=env!("CARGO_PKG_DESCRIPTION"), long_about = None)]
 
 struct Args {
     /// Include uppercase alphabets. Default false
